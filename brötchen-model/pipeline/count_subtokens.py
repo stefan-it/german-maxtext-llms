41c39beb9de8a1edf3d9f413c0c62df1976947a0
Stefan Schweter <stefan@schweter.it>
2025-12-03 17:04:47 +0100

    feat: add script for retrieving total subword stats across all data

---
+++ brötchen-model/pipeline/count_subtokens.py
@@ -0,0 +1,55 @@
+import argparse
+import pandas as pd
+from tabulate import tabulate
+import yaml
+
+from pathlib import Path
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="DataTrove Pipeline for creating SLM Datasets"
+    )
+    parser.add_argument(
+        '--config',
+        type=str,
+        help='Path to yaml configuration file'
+    )
+
+    args = parser.parse_args()
+
+    with open(args.config, 'r') as f:
+        config = yaml.safe_load(f)
+
+    dataset_name_stats = {}
+    headers = ["Dataset name", "Total Subtokens"]
+    rows = []
+
+    for dataset_name, dataset_config in config['datasets'].items():
+        #print(dataset_name, dataset_config)
+        dataset_output_path = Path(dataset_config["output_path"]) / dataset_name
+
+        total_subtokens = 0
+
+        if not dataset_output_path.exists():
+            continue
+
+        for parquet_file in dataset_output_path.iterdir():
+            if not parquet_file.name.endswith(".parquet"):
+                continue
+
+            df = pd.read_parquet(parquet_file)
+            total_subtokens += df['metadata'].apply(lambda x: x.get('subtokens', 0)).sum()
+        
+        dataset_name_stats[dataset_name] = total_subtokens
+
+        rows.append([dataset_name, total_subtokens])
+
+    # Add total row
+    rows.append(["All", sum(dataset_name_stats.values())])
+
+    print(tabulate(rows, headers=headers, tablefmt="github"))
+
+
+if __name__ == "__main__":
+    main()
